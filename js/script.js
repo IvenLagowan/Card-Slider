a9f27054f42ffcb80801450385925cb3b534acb7
iven lagowan <johndoe@example.com>
2023-09-04 03:34:09 +0900

    new files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 2,
+    spaceBetween: 25,
+    loop: true,
+    fade: 'true',
+    gragCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
